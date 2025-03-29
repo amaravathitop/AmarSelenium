165f25f9787a16b3e97c766beec65374c30f001c
amaravathi <amaravathi.sa@gmail.com>
2025-03-29 19:34:33 +0530

    Selenium day1 assignements added

---
+++ MavenProject/src/main/java/week1/day1/ChromeVarDeclation.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class ChromeVarDeclation {
+	
+	public static void main(String args[])
+	{
+		float version= 91.0f;
+		String developer="Google";
+		boolean isBeta= false;
+		int releaseYear=2008;
+	    char shortcutKey='C';
+	    
+		System.out.println("version detail: " +version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+	}
+
+}
+
---
+++ MavenProject/src/main/java/week1/day1/ShortCut.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class ShortCut {
+	public static void main(String[] args) {
+		System.out.println("This is for shortcut");
+	}
+
+}
